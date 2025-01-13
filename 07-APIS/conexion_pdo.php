71b754c441ae181df5b1de2156b3e837f584d96d
DRojasRamirez <DanielRojasR96@gmail.com>
2025-01-13 14:42:59 +0100

    Clase 13/01

---
+++ 07-APIS/conexion_pdo.php
@@ -0,0 +1,16 @@
+<?php
+    $_servidor = "localhost";
+    $_usuario = "estudiante";
+    $_contrasena = "estudiante";
+    $_bd = "animes_bd";
+
+    try {
+        $_conexion = new PDO("mysql:host=$_servidor;dbname=$_bd", $_usuario, $_contrasena);
+        $_conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch (PDOExeption $e){
+        die("Error de conexion: " . $e -> getMessage());
+    }
+
+
+?>
